7754e340d8eb0a14f8ac6fe7010697a1a4c49c33
Leonardo Toccaceli <leotoccaceli@gmail.com>
2019-09-05 12:26:33 -0300

    filterBy fixed

--- kindle.js
+++ kindle.js
@@ -6,12 +6,15 @@ class Kindle {
 		this._next = null;
 		this._last = null;
 		this._library = [];
+		this._read = [];
+		this._unread = [];
 	}
 	add(eBook) {
 		if (this._library.some(x => x.title == eBook.title)) {
 			console.warn(`"${eBook.title}" already exists in library`);
 		} else {
 			this._library.push(eBook);
+			this._unread.push(eBook);
 			this.notYetReadBooks++;
 			if (this._current === null) {
 				this._current = eBook;
@@ -39,6 +42,10 @@ class Kindle {
 		} else {
 			this._current.read = true;
 			this._current.readDate = Date.now();
+			this._read.push(
+				...this.library.filter(x => x.title === this._current.title)
+			);
+			this._unread = this.library.filter(x => x.title !== this._current.title);
 			this._last = this._current;
 			this._current = this._next;
 			this._next = this._library.find(x => !x.read && x != this._current);
@@ -48,53 +55,36 @@ class Kindle {
 	}
 	sortBy(criteria) {
 		if (criteria === 'author') {
-			return this._library.sort((a, b) =>
+			return this.library.sort((a, b) =>
 				a.author > b.author ? 1 : b.author > a.author ? -1 : 0
 			);
 		} else if (criteria === 'title') {
-			return this._library.sort((a, b) =>
+			return this.library.sort((a, b) =>
 				a.title > b.title ? 1 : b.title > a.title ? -1 : 0
 			);
 		} else {
-			return "only support 'author' or 'title' criteria";
+			return console.error("only support 'author' or 'title' criteria");
 		}
 	}
 	get library() {
-		return { ...this._library };
+		return [
+			...this._library.map(function(ebook) {
+				delete ebook.read;
+				delete ebook.readDate;
+				return ebook;
+			})
+		];
 	}
 	get size() {
 		return this._library.length;
 	}
 
 	filterBy(criteria) {
-		let auxArr = this._library.slice();
-
-		if (criteria === 'read') {
-			let arrToReturn = auxArr.filter(x => x.read === true);
-			for (const e of arrToReturn) {
-				delete e.read;
-				delete e.readDate;
-			}
-
-			if (arrToReturn.length === 0) {
-				return console.log('You have no items that match the selected filters');
-			} else {
-				return arrToReturn;
-			}
-		} else if (criteria === 'unread') {
-			let arrToReturn = auxArr.filter(x => x.read === false);
-			for (const e of arrToReturn) {
-				delete e.read;
-				delete e.readDate;
-			}
-			if (arrToReturn.length === 0) {
-				return console.log('You have no items that match the selected filters');
-			} else {
-				return arrToReturn;
-			}
-		} else {
-			return console.log('You have no items that match the selected filters');
-		}
+		return criteria === 'read'
+			? this._read
+			: criteria === 'unread'
+			? this._unread
+			: console.error('You have no items that match the selected filters');
 	}
 }
 
